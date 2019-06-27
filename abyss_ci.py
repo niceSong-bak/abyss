6ece054afc478bfbbf51ef077eeaee431f317ae7
aaron <aaronlzxian@163.com>
2019-06-27 16:06:53 +0800

    根据ref判断环境

--- abyss_ci.py
+++ abyss_ci.py
@@ -2,28 +2,33 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import re
 from abyss.docker.ci_docker import CIDocker
 from abyss import logger as LOG, config_parser
 
 __author__ = "Jude"
 
-def transfer(pipe):
-    if pipe == "docker_prod" or pipe == "docker_aws" or pipe == "docker_ocean" or pipe == "release":
+pattern = re.compile('^refs/((heads)/.+|(tags)/.+)$')
+release = 'tags'
+beta = 'heads'
+
+def transfer(git_ref):
+
+    p = pattern.findall(git_ref)
+    if len(p) < 1:
+        LOG.error("unknown git_ref: " + git_ref)
+        sys.exit(1)
+
+    if release in p[0]:
         return config_parser.CI_BUILD_COMMAND_RELEASE
-    elif pipe == "docker_beta" or pipe == "beta" or pipe == "fucker":
+    elif beta in p[0]:
         return config_parser.CI_BUILD_COMMAND_BETA
     else:
-        LOG.error("unknown pipe: " + pipe)
+        LOG.error("unknown git_ref: " + git_ref)
         sys.exit(1)
 
 
 if __name__ == "__main__":
-    if "pipe" in os.environ:
-        pipe = os.environ['pipe']
-    else:
-        LOG.error("Missing pipe")
-        sys.exit(1)
-
     if "WORKSPACE" in os.environ:
         workplace = os.environ['WORKSPACE']
     else:
@@ -41,18 +46,15 @@ if __name__ == "__main__":
     else:
         LOG.error("Missing git_ref")
         sys.exit(1)
+    pipe = transfer(git_ref)
 
     LOG.debug(pipe)
-
-    _pipe = transfer(pipe)
-
-    LOG.debug(_pipe)
 
     if not CIDocker(
         workplace=workplace,
         git_url=git_url,
         git_ref=git_ref,
-        pipe=_pipe
+        pipe=pipe
     ).ci_process():
         LOG.big_log_start("Jenkins Job Failed!")
         sys.exit(1)
