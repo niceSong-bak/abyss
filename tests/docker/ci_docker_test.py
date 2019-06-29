52260abcb6adcf4175547cb6353472cf7a6bb8f2
aaron <aaronlzxian@163.com>
2019-06-29 13:44:15 +0800

    test

--- tests/docker/ci_docker_test.py
+++ tests/docker/ci_docker_test.py
@@ -64,8 +64,9 @@ class TestBeta(unittest.TestCase):
         CIDocker(
             workplace=directory,
             # git_url="git@gitee.com:jinuotech/Tristana-AdminFrontend.git",
-            git_url="git@gitee.com:floozy/springdemo.git",
-            git_ref="refs/heads/devww",
+            # git_url="git@gitee.com:floozy/springdemo.git",
+            git_url="https://gitee.com/jinuotech/Paymire.git",
+            git_ref="refs/heads/dev",
             pipe="beta",
             commits=commit_json
         ).ci_process()
